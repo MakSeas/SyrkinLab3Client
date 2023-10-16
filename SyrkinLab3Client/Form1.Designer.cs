ccf08495a9bc00120519bc5af9ee7d714de80ff2
Im the User <Im the User@THECOMP>
2023-10-17 02:51:37 +0700

    Добавьте файлы проекта.

---
+++ SyrkinLab3Client/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SyrkinLab3Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.usernameButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusText = new System.Windows.Forms.Label();
+            this.usernameOnlineLabel = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.usernameButton);
+            this.tabPage1.Controls.Add(this.usernameLabel);
+            this.tabPage1.Controls.Add(this.usernameInput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // usernameButton
+            // 
+            this.usernameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.usernameButton.Location = new System.Drawing.Point(287, 199);
+            this.usernameButton.Name = "usernameButton";
+            this.usernameButton.Size = new System.Drawing.Size(207, 55);
+            this.usernameButton.TabIndex = 2;
+            this.usernameButton.Text = "Войти";
+            this.usernameButton.UseVisualStyleBackColor = true;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.usernameLabel.Location = new System.Drawing.Point(223, 129);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(356, 31);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Введите имя пользователя";
+            // 
+            // usernameInput
+            // 
+            this.usernameInput.Location = new System.Drawing.Point(204, 173);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(375, 20);
+            this.usernameInput.TabIndex = 0;
+            this.usernameInput.Text = "  ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.statusText);
+            this.tabPage2.Controls.Add(this.usernameOnlineLabel);
+            this.tabPage2.Controls.Add(this.sendButton);
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusText.ForeColor = System.Drawing.Color.Green;
+            this.statusText.Location = new System.Drawing.Point(10, 38);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(56, 18);
+            this.statusText.TabIndex = 5;
+            this.statusText.Text = "Online";
+            // 
+            // usernameOnlineLabel
+            // 
+            this.usernameOnlineLabel.AutoSize = true;
+            this.usernameOnlineLabel.BackColor = System.Drawing.Color.Transparent;
+            this.usernameOnlineLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.usernameOnlineLabel.Location = new System.Drawing.Point(8, 13);
+            this.usernameOnlineLabel.Name = "usernameOnlineLabel";
+            this.usernameOnlineLabel.Size = new System.Drawing.Size(191, 25);
+            this.usernameOnlineLabel.TabIndex = 4;
+            this.usernameOnlineLabel.Text = "Имя пользователя";
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sendButton.Location = new System.Drawing.Point(663, 333);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(112, 70);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Отправить";
+            this.sendButton.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(26, 333);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(631, 70);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Image = global::SyrkinLab3Client.Properties.Resources.Фиолет1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 321);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(786, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SyrkinLab3Client.Properties.Resources._1674271088_catherineasquithgallery_com_p_temno_serii_fon_1920x1080_foto_1;
+            this.pictureBox2.Location = new System.Drawing.Point(-4, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(798, 10);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button usernameButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label usernameOnlineLabel;
+        private System.Windows.Forms.Label statusText;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
